d34bd3982d4e789753f5f1027f7e4cd8ba6d2c31
jaydeep.sankpal@gmail.com <jaydeep.sankpal@gmail.com>
2017-05-11 08:34:31 +0530

    Servlet Added

---
+++ src/main/java/com/mkyong/controller/Register.java
@@ -0,0 +1,24 @@
+package com.mkyong.controller;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet {  
+public void doGet(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+  
+response.setContentType("text/html");  
+PrintWriter out = response.getWriter();  
+          
+String n=request.getParameter("userName");  
+String p=request.getParameter("userPass");  
+String e=request.getParameter("userEmail");  
+String c=request.getParameter("userCountry");  
+     response.getWriter().write(n+" "+p);     
+}
+  
+}  
